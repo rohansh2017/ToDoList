e421fa9b363d13622016b9f894e76940d0df7ca3
Rohan Sharma <rohansh2017@gmail.com>
2023-12-26 13:49:12 -0500

    Add clear all button, refined search, and functionality

--- script.js
+++ script.js
@@ -12,9 +12,9 @@ function addTask(){
         let li = document.createElement("li");
         li.innerHTML = inputBox.value;
         if (dateBox != null)
-            li.innerHTML += "\t" + dateBox.value;
+            li.innerHTML += ' ' + dateBox.value;
         if (timeBox != null)
-            li.innerHTML += "\t" + timeBox.value;
+            li.innerHTML += ' ' + timeBox.value;
         listContainer.appendChild(li);
         let span = document.createElement("span");
         span.innerHTML = "\u00d7";
@@ -22,17 +22,17 @@ function addTask(){
     }
     inputBox.value = "";
     dateBox.value = "";
-    // saveData();
+    saveData();
 }
 
 listContainer.addEventListener("click", function(e){
     if (e.target.tagName === "LI"){
         e.target.classList.toggle("checked");
-        // saveData();
+        saveData();
     }
     else if (e.target.tagName === "SPAN"){
         e.target.parentElement.remove();
-        // saveData();
+        saveData();
     }
 }, false);
 
@@ -47,12 +47,28 @@ function showTask(){
 function searchEvent(){
     const searchBox = document.getElementById("search-box");
     const searchContainer = document.getElementById("search-container");
-    if (listContainer.contains(searchBox))
-        searchContainer.appendChild(searchBox);
+    let liElements = listContainer.getElementsByTagName("li");
+    let searchedItems = [];
+    for(let i = 0; i < liElements.length; i++){
+        if(liElements[i].innerHTML.includes(searchBox.value)){
+            searchedItems.push(liElements[i].innerHTML);
+        }
+    }
+    searchContainer.innerHTML = '';
+    searchedItems.forEach(function(item){
+        let x = document.createElement("li");
+        x.innerHTML = item;
+        searchContainer.appendChild(x);
+    });
 }
-// function clearData(){
-//     listContainer.innerHTML = localStorage.removeItem("data");
+function clearAllEvents(){
+    listContainer.innerHTML = "";
+    saveData();
+}
+// function clearData() {
+//     localStorage.clear();
 // }
-//clearData();
 showTask();
 searchEvent();
+document.getElementById("clear-all-btn").addEventListener("click", clearAllEvents, false);
+//clearData();
